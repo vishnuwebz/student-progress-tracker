c6ab3f0282141fc86ef967e69d375fe363dc27b1
Vishnu G <vishnugkannath@gmail.com>
2026-03-07 16:34:22 +0530

    Module 1 - setup and hello world

---
+++ main.py
@@ -0,0 +1,9 @@
+def main():
+    print("==================================")
+    print("Student Progress Tracker (CLI)")
+    print("==================================")
+    print("Welcome! This is the starting point of our project.")
+    print("In upcoming modules, we will add features step by step.")
+
+if __name__ == "__main__":
+    main()
