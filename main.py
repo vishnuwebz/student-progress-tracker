9b772df0270a6d202a0097f87c190379d746b35b
Vishnu G <vishnugkannath@gmail.com>
2026-03-07 21:00:54 +0530

    Module 2- variables, input and type conversion

--- main.py
+++ main.py
@@ -2,8 +2,24 @@ def main():
     print("==================================")
     print("Student Progress Tracker (CLI)")
     print("==================================")
-    print("Welcome! This is the starting point of our project.")
-    print("In upcoming modules, we will add features step by step.")
+    print("\n[Module 2] Collecting basic student details...\n")
+
+    # getting input from the user (*all inputs first come as strings)
+    student_name = input("Enter student name: ")
+    age_input = input("Enter student age: ")
+    course = input("Enter course name: ")
+
+    # type conversion (string -> int) with a simple assumption it is valid
+    age = int(age_input)
+
+    years_to_25 = 25 - age
+
+    print("\n--- Student Summary ---")
+    print("Name :", student_name)
+    print("Age :", age)
+    print("Course :", course)
+    print(f"{student_name} will be 25 in {years_to_25} year(s)")
+
 
 if __name__ == "__main__":
     main()
